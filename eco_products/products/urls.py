394758ea47121a7b3c4c471c6d4f520a178214d6
maxim <maximiglinrest@gmail.com>
2021-10-08 22:10:47 +0300

    detail category

--- eco_products/products/urls.py
+++ eco_products/products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import get_shop_page
+from .views import get_shop_page, category_detail_page
 from .api import CategoryApiView, CategoryDetailApi, ProductApiView, ProductDetailApi
 
 
 urlpatterns = [
     path('shop', get_shop_page, name="shop"),
+    path('category/<str:slug>', category_detail_page, name="category"),
 
     path('api/category', CategoryApiView.as_view()),
     path('api/category/<str:slug>', CategoryDetailApi.as_view()),
