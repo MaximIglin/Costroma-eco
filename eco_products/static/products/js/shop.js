024879aed19bc42d813cb38c18ed47c3c4da73bd
lehnele <lehnele@gmail.com>
2021-10-10 21:52:44 +0300

    category anim

--- eco_products/static/products/js/shop.js
+++ eco_products/static/products/js/shop.js
@@ -1,1 +1,14 @@
-console.log("fkbjbkjdsbf")
+const categories = document.querySelectorAll('.category_button')
+
+const slides = document.querySelectorAll('.slide')
+
+for (const category_button of categories) {
+    category_button.addEventListener('mouseover', () => {
+        category_button.classList.add('active_categoty')
+    })
+
+    category_button.addEventListener('mouseout', () => {
+        category_button.classList.remove('active_categoty')
+    })
+
+}
