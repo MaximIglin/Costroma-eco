aa5c52a27d1c392062949b0f7c8972630cac25d0
lehnele <lehnele@gmail.com>
2021-10-16 01:40:39 +0300

    popup

--- eco_products/static/products/js/shop.js
+++ eco_products/static/products/js/shop.js
@@ -1,3 +1,4 @@
+//Dogadkin: category menu animation
 const categories = document.querySelectorAll('.category_button')
 
 const slides = document.querySelectorAll('.slide')
@@ -17,7 +18,100 @@ for (const category_button of categories) {
 
 
 
+//Dogadkin: popup
+const popupLinks = document.querySelectorAll('.popup-link')
+const body = document.querySelector('body')
+const lockPadding = document.querySelectorAll('.lock-padding')
 
+let unlock = true
+
+const timeout = 800
+
+if (popupLinks.length > 0) {
+    for (let index = 0; index < popupLinks.length; index++) {
+        const popupLink = popupLinks[index]
+        popupLink.addEventListener("click", function (e) {
+            const popupName = popupLink.getAttribute('href').replace('#','')
+            const currentPopup = document.getElementById(popupName)
+            popupOpen(currentPopup)
+            e.preventDefault()
+        })
+    }
+}
+const popupCloseIcon = document.querySelectorAll('.close-popup')
+if (popupCloseIcon.length > 0) {
+    for (let index = 0; index < popupCloseIcon.length; index++) {
+        const el = popupCloseIcon[index]
+        el.addEventListener("click", function (e) {
+            popupClose(el.closest('.popup'))
+            e.preventDefault()
+        })
+    }
+}
+
+function popupOpen(currentPopup) {
+    if (currentPopup && unlock){
+        const popupActive = document.querySelector('.popup.open')
+        if (popupActive) {
+            popupClose(popupActive, false)
+        } else {
+            bodyLock();
+        }
+        currentPopup.classList.add('open')
+        currentPopup.addEventListener('click', function (e) {
+            if (!e.target.closest('popup__content')) {
+                popupClose(e.target.closest('.popup'))
+            }
+        })
+    }
+}
+function popupClose (popupActive, doUnlock = true) {
+    if (unlock) {
+        popupActive .classList.remove('open')
+        if(doUnlock) {
+            bodyUnlock();
+        }
+    }
+}
+
+function bodyLock() {
+    const lockPaddingValue = window.innerWidth - document.querySelector('.main_shop').offsetWidth + 'px'
+
+    if(lockPadding.length > 0) {
+        for (let index = 0; index < lockPadding.length; index++) {
+            const el = lockPadding[index]
+            el.style.paddingRight = lockPaddingValue
+        }
+    }
+    body.style.paddingRight = lockPaddingValue
+    body.classList.add('lock')
+
+    unlock = false
+    setTimeout(() =>{
+        unlock = true
+    }, timeout)     
+}
+
+function bodyUnlock() {
+    setTimeout(() => {
+        for (let index = 0; index < lockPadding.length; index++) {
+            const el = lockPadding[index]
+            el.style.paddingRight = '0px'
+        }
+        body.style.paddingRight = '0px'
+        body.classList.remove('lock')
+    }, timeout)
+
+    unlock = false
+    setTimeout(() =>{
+        unlock = true
+    }, timeout)    
+}
+
+
+
+//Iglin: counter
+const qtt = document.querySelectorAll('.qtt')
 let cart = {
 }
 
@@ -41,6 +135,8 @@ document.addEventListener("DOMContentLoaded", () => {
         addCartBtn.addEventListener("click", () => {
             cart[addCartBtn.id]["quantity"] += 1;
             document.cookie = encodeURIComponent("cart") + '=' + JSON.stringify(cart)
+            qtt[addCartBtn.id].innerHTML = cart[addCartBtn.id]["quantity"]
+            console.log(qtt[addCartBtn.id])
         })
     }
 
@@ -49,12 +145,13 @@ document.addEventListener("DOMContentLoaded", () => {
         rmCartBtn.addEventListener("click", () => {
             cart[rmCartBtn.id]["quantity"] = cart[rmCartBtn.id]["quantity"] - 1;
             document.cookie = encodeURIComponent("cart") + '=' + JSON.stringify(cart)
+            qtt[rmCartBtn.id].innerHTML = cart[rmCartBtn.id]["quantity"]
+            console.log(qtt[rmCartBtn.id])
         })
 
     }
 
 })
-
 
 
 
