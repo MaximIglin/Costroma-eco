48ae878b1b39ef4c84a84e544d9eda4bc94aa8d9
lehnele <lehnele@gmail.com>
2021-10-16 21:26:08 +0300

    home cart

--- eco_products/static/products/js/cart.js
+++ eco_products/static/products/js/cart.js
@@ -1,57 +1,69 @@
-const cartLink = document.querySelector('.cart')
-const detailCart = document.querySelector('.detail_cart')
+document.addEventListener("DOMContentLoaded", () => {
 
-document.addEventListener('DOMContentLoaded', () => {
-    cartLink.addEventListener('click', () => {
-        cartOpen(detailCart)
-    })
+    const cartLink = document.querySelector('.cart');
+    const detailCart = document.querySelector('.detail_cart')
+    const cartContent = document.querySelector('.cart__content')
 
-    function cartOpen(detailCart) {
-        bodyLock();
-        detailCart.classList.add('open')
-        detailCart.addEventListener('click', function (e) {
-            if (!e.target.closest('cart__content')) {
-                console.log(!e.target.closest('cart__content'))
-                cartClose(e.target.closest('.detail_cart'))
-            }
+      cartLink.addEventListener("click", () => {
+        cartOpen(detailCart);
+        fetch("http://localhost:8000/api/cart_detail")
+        .then((response) => response.json())
+        .then((data) => (products = data))
+        .then(() => {
+            products.forEach((product) => {
+                cartContent.innerHTML += `<h1>${product.name}</h1>`
+            })
         })
-    }
-    function cartClose(detailCart) {
-        detailCart.classList.remove('open')
-        bodyUnlock();
-    }
+      });
 
-    function bodyLock() {
-        const lockPaddingValue = window.innerWidth - document.querySelector('.main_shop').offsetWidth + 'px'
+      function cartOpen(detailCart) {
+        bodyLock();
+        detailCart.classList.add("open");
+        detailCart.addEventListener("click", function (e) {
+          if (!e.target.closest("cart__content")) {
+            console.log(!e.target.closest("cart__content"));
+            cartClose(e.target.closest(".detail_cart"));
+            cartContent.innerHTML = ''
+          }
+        });
+      }
+      function cartClose(detailCart) {
+        detailCart.classList.remove("open");
+        bodyUnlock();
+      }
+
+      function bodyLock() {
+        const lockPaddingValue = window.innerWidth - document.querySelector(".main_shop").offsetWidth + "px";
 
         if (lockPadding.length > 0) {
-            for (let index = 0; index < lockPadding.length; index++) {
-                const el = lockPadding[index]
-                el.style.paddingRight = lockPaddingValue
-            }
+          for (let index = 0; index < lockPadding.length; index++) {
+            const el = lockPadding[index];
+            el.style.paddingRight = lockPaddingValue;
+          }
         }
-        body.style.paddingRight = lockPaddingValue
-        body.classList.add('lock')
+        body.style.paddingRight = lockPaddingValue;
+        body.classList.add("lock");
 
-        unlock = false
+        unlock = false;
         setTimeout(() => {
-            unlock = true
-        }, timeout)
-    }
+          unlock = true;
+        }, timeout);
+      }
 
-    function bodyUnlock() {
+      function bodyUnlock() {
         setTimeout(() => {
-            for (let index = 0; index < lockPadding.length; index++) {
-                const el = lockPadding[index]
-                el.style.paddingRight = '0px'
-            }
-            body.style.paddingRight = '0px'
-            body.classList.remove('lock')
-        }, timeout)
+          for (let index = 0; index < lockPadding.length; index++) {
+            const el = lockPadding[index];
+            el.style.paddingRight = "0px";
+          }
+          body.style.paddingRight = "0px";
+          body.classList.remove("lock");
+        }, timeout);
 
-        unlock = false
+        unlock = false;
         setTimeout(() => {
-            unlock = true
-        }, timeout)
-    }
-})
+          unlock = true;
+        }, timeout);
+      }
+    
+});
