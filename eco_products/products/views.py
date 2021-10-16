de0c4440ae6069e4d8a9f0374bcc105ec94f86e4
maxim <maximiglinrest@gmail.com>
2021-10-16 20:28:24 +0300

    cart_view

--- eco_products/products/views.py
+++ eco_products/products/views.py
@@ -19,17 +19,8 @@ def get_shop_page(request):
 
         cart_products_id = [int(products_id[i]) for i in range(0, len(products_id)) if int(products_quantity[i]) != 0 ]
         cart_products_qty = [int(qty) for qty in products_quantity if int(qty) !=0 ]
-
-
         cart_products_queryset = Product.objects.filter(id__in=cart_products_id)
-        print(cart_products_queryset)
-        print(cart)
-        print(len(products_id))
-        print(len(products_quantity))
-        print(len(cart_products_id))
-        print(len(cart_products_qty))
-        print(f"id продуктов  -- -- -- {cart_products_id}")
-        print(f"Количество продуктов {cart_products_qty}")
+        context["cart_products"] = cart_products_queryset
         return render(request, "shop.html", context)
     except KeyError:    
         return render(request, "shop.html", context)
