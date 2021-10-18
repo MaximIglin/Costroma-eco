985c21ca085934b9ef90deeb4ea270932ae78e83
lehnele <lehnele@gmail.com>
2021-10-18 03:59:32 +0300

    fix

--- eco_products/landing/views.py
+++ eco_products/landing/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from products.models import Category
 
+from products.models import Product
+
 
 def render_landing_page(request):
     categories = Category.objects.all()
-    return render(request, 'home.html', {'categories':categories})
+    products = Product.objects.all()[:5]
+    return render(request, 'home.html', {'categories':categories, "products": products})
